7dbe94e4bbe81cce9e5325049131f11cc3b92f21
mmolta <mcmolta10@gmail.com>
2022-08-03 13:48:40 -0400

    generic layerColor fnc

--- js/map/secondaryMapLayers.js
+++ js/map/secondaryMapLayers.js
@@ -1,4 +1,3 @@
-// @update for change to 1 generic fillColor fnc
 const pevColors = [
   '#f0f9e8',
   '#ccebc5',
@@ -27,53 +26,6 @@ const wpjobColors = [
   '#01665e'
 ]
 
-
-// make pev layers
-const pevStyle = (vals, layer) => {
-  let [a, b, c, d, e, f] = vals
-  return ['step',
-    ['get', layer],
-    '#f0f9e8',
-    a, '#ccebc5',
-    b, '#a8ddb5',
-    c, '#7bccc4',
-    d, '#4eb3d3',
-    e, '#2b8cbe',
-    f, '#08589e'
-  ]
-}
-
-// make wp layers
-const wpStyle = (vals, layer) => {
-  let [a, b, c, d, e, f] = vals
-  return ['step',
-    ['get', layer],
-    '#ffffd4',
-    a, '#fed976',
-    b, '#feb24c',
-    c, '#fd8d3c',
-    d, '#fc4e2a',
-    e, '#e31a1c',
-    f, '#b10026'
-  ]
-}
-
-// make wpjob layers
-const wpjobStyle = (vals, layer) => {
-  let [a, b, c, d, e, f] = vals
-  return ['step',
-    ['get', layer],
-    '#8c510a',
-    a, '#d8b365',
-    b, '#f6e8c3',
-    c, '#f5f5f5',
-    d, '#c7eae5',
-    e, '#5ab4ac',
-    f, '#01665e'
-  ]
-}
-
-// @update: can abstract ^ to this:
 const makeFillColor = (vals, layer, colors) => {
   const [a, b, c, d, e, f] = vals
   const [g, h, i, j, k, l, m] = colors
@@ -90,278 +42,224 @@ const makeFillColor = (vals, layer, colors) => {
   ]  
 }
 
-
 // Obj to reference all geographic layer specs
 const layerSpecs = {
   'DVRPC-CurrentPEV-Pop': {
-    id: 'DVRPC-CurrentPEV-Pop',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: pevStyle([.086,.138,.2085,.303,.45,.71], 'CuPEVPop')
+    fillColor: makeFillColor([.086,.138,.2085,.303,.45,.71], 'CuPEVPop', pevColors)
   },
   'DVRPC-CurrentPEV-HH': {
-    id: 'DVRPC-CurrentPEV-HH',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: pevStyle([.229,.355,.548,.7783,1.16,1.89], 'CuPEV_HU')
+    fillColor: makeFillColor([.229,.355,.548,.7783,1.16,1.89], 'CuPEV_HU', pevColors)
   },
   'DVRPC-CurrentPEV-SM': {
-    id: 'DVRPC-CurrentPEV-SM',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: pevStyle([2.98,5.25,8.01,11.9,17.65,33.71], 'CuPEV_SM')
+    fillColor: makeFillColor([2.98,5.25,8.01,11.9,17.65,33.71], 'CuPEV_SM', pevColors)
   },
   'DVRPC-CurrentPEV-Veh': {
-    id: 'DVRPC-CurrentPEV-Veh',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: pevStyle([.0016,.0024,.0032,.0044,.0065,.0103], 'PerCuPEV')
+    fillColor: makeFillColor([.0016,.0024,.0032,.0044,.0065,.0103], 'PerCuPEV', pevColors)
   },
   'DVRPC-FuturePEV-BG': {
-    id: 'DVRPC-FuturePEV-BG',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: pevStyle([4.49,7.87,12.87,21.39,35.52,72.51], 'FutPEV')
+    fillColor: makeFillColor([4.49,7.87,12.87,21.39,35.52,72.51], 'FutPEV', pevColors)
   },
   'DVRPC-FuturePEV-Pop': {
-    id: 'DVRPC-FuturePEV-Pop',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: pevStyle([0.44,0.72,1.13,1.69,2.54,4.75], 'FuPEVPop')
+    fillColor: makeFillColor([0.44,0.72,1.13,1.69,2.54,4.75], 'FuPEVPop', pevColors)
   },
   'DVRPC-FuturePEV-HH': {
-    id: 'DVRPC-FuturePEV-HH',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: pevStyle([1.25,1.94,2.94,4.31,6.66,12.27], 'FuPEV_HU')
+    fillColor: makeFillColor([1.25,1.94,2.94,4.31,6.66,12.27], 'FuPEV_HU', pevColors)
   },
   'DVRPC-FuturePEV-SM': {
-    id: 'DVRPC-FuturePEV-SM',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: pevStyle([26.97,45.94,67.38,92.56,134.82,238.44], 'FUPEV_SM')
+    fillColor: makeFillColor([26.97,45.94,67.38,92.56,134.82,238.44], 'FUPEV_SM', pevColors)
   },
   'DVRPC-FuturePEV-Veh': {
-    id: 'DVRPC-FuturePEV-Veh',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: pevStyle([.0111,.0149,.01983,.02648,.03766,.06804], 'PerFuPEV')
+    fillColor: makeFillColor([.0111,.0149,.01983,.02648,.03766,.06804], 'PerFuPEV', pevColors)
   },
   'DVRPC-FC-KD-SM': {
-    id: 'DVRPC-FC-KD-SM',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: wpStyle([12.29,28.98,56.37,100.15,164.75,321.26], 'FC_KD_SM')
+    fillColor: makeFillColor([12.29,28.98,56.37,100.15,164.75,321.26], 'FC_KD_SM', wpColors)
   },
   'DVRPC-FC-KD-JB': {
-    id: 'DVRPC-FC-KD-JB',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: wpjobStyle([.054,.074,.0898,.1046,.1228,.1517], 'FC_KD_JB')
+    fillColor: makeFillColor([.054,.074,.0898,.1046,.1228,.1517], 'FC_KD_JB', wpjobColors)
   },
   'DVRPC-FC-CE-SM': {
-    id: 'DVRPC-FC-CE-SM',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: wpStyle([1.802,4.2266,8.1766,14.1452,23.9576,44.3198], 'FC_CE_SM')
+    fillColor: makeFillColor([1.802,4.2266,8.1766,14.1452,23.9576,44.3198], 'FC_CE_SM', wpColors)
   },
   'DVRPC-FC-CE-JB': {
-    id: 'DVRPC-FC-CE-JB',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: wpjobStyle([.0083,.0108,.0128,.0145,.0169,.0209], 'FC_CE_JB')
+    fillColor: makeFillColor([.0083,.0108,.0128,.0145,.0169,.0209], 'FC_CE_JB', wpjobColors)
   },
   'DVRPC-PC-KD-SM': {
-    id: 'DVRPC-PC-KD-SM',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: wpStyle([8.52,20.03,39.14,69.46,114.29,222.67], 'PC_KD_SM')
+    fillColor: makeFillColor([8.52,20.03,39.14,69.46,114.29,222.67], 'PC_KD_SM', wpColors)
   },
   'DVRPC-PC-KD-JB': {
-    id: 'DVRPC-PC-KD-JB',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: wpjobStyle([.0378,.0512,.0623,.0724,.0853,.1052], 'PC_KD_JB')
+    fillColor: makeFillColor([.0378,.0512,.0623,.0724,.0853,.1052], 'PC_KD_JB', wpjobColors)
   },
   'DVRPC-PC-CE-SM': {
-    id: 'DVRPC-PC-CE-SM',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: wpStyle([1.2,2.83,5.48,9.5,15.96,29.68], 'PC_CE_SM')
+    fillColor: makeFillColor([1.2,2.83,5.48,9.5,15.96,29.68], 'PC_CE_SM', wpColors)
   },
   'DVRPC-PC-CE-JB': {
-    id: 'DVRPC-PC-CE-JB',
     sourceLayer: 'dvrpc_pev_bg',
-    fillColor: wpjobStyle([.0056,.0072,.0085,.0097,.0113,.014], 'PC_CE_JB')
+    fillColor: makeFillColor([.0056,.0072,.0085,.0097,.0113,.014], 'PC_CE_JB', wpjobColors)
   },
 
   // Pennsylvannia 
   'PA-CurrentPEV-BG': {
-    id: 'PA-CurrentPEV-BG',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([0.98,1.64,2.66,4.22,6.47,11.83], 'CurPEV')
+    fillColor: makeFillColor([0.98,1.64,2.66,4.22,6.47,11.83], 'CurPEV', pevColors)
   },
   'PA-CurrentPEV-Pop': {
-    id: 'PA-CurrentPEV-Pop',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([0.07,0.1,0.14,0.19,0.28,0.71], 'CuPEVPop')
+    fillColor: makeFillColor([0.07,0.1,0.14,0.19,0.28,0.71], 'CuPEVPop', pevColors)
   },
   'PA-CurrentPEV-HH': {
-    id: 'PA-CurrentPEV-HH',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([0.183,0.253,0.347,0.48,0.69,1.15], 'CuPEV_HU')
+    fillColor: makeFillColor([0.183,0.253,0.347,0.48,0.69,1.15], 'CuPEV_HU', pevColors)
   },
   'PA-CurrentPEV-SM': {
-    id: 'PA-CurrentPEV-SM',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([0.21,1.07,2.91,5.41,9.62,19.36], 'CuPEV_SM')
+    fillColor: makeFillColor([0.21,1.07,2.91,5.41,9.62,19.36], 'CuPEV_SM', pevColors)
   },
   'PA-CurrentPEV-Veh': {
-    id: 'PA-CurrentPEV-Veh',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([0.001,0.0015,0.002,0.0028,0.0038,0.0066], 'PerCuPEV')
+    fillColor: makeFillColor([0.001,0.0015,0.002,0.0028,0.0038,0.0066], 'PerCuPEV', pevColors)
   },
   // @update missing data
   'PA-FuturePEV-BG': {
-    id: 'PA-FuturePEV-BG',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([], 'FutPEV')
+    fillColor: makeFillColor([], 'FutPEV', pevColors)
   },
   'PA-FuturePEV-Pop': {
-    id: 'PA-FuturePEV-Pop',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([0.73,1.12,1.53,2.06,2.93,5.2], 'FuPEVPop')
+    fillColor: makeFillColor([0.73,1.12,1.53,2.06,2.93,5.2], 'FuPEVPop', pevColors)
   },
   'PA-FuturePEV-HH': {
-    id: 'PA-FuturePEV-HH',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([1.87,2.78,3.72,5.02,7.16,12.91], 'FuPEV_HU')
+    fillColor: makeFillColor([1.87,2.78,3.72,5.02,7.16,12.91], 'FuPEV_HU', pevColors)
   },
   'PA-FuturePEV-SM': {
-    id: 'PA-FuturePEV-SM',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([2.63,13.94,37.71,69.56,120.07,231.87], 'FUPEV_SM')
+    fillColor: makeFillColor([2.63,13.94,37.71,69.56,120.07,231.87], 'FUPEV_SM', pevColors)
   },
   'PA-FuturePEV-Veh': {
-    id: 'PA-FuturePEV-Veh',
     sourceLayer: 'pa_pev_bg',
-    fillColor: pevStyle([0.0135,0.0177,0.0225,0.0291,0.0409,0.0716], 'PerFuPEV')
+    fillColor: makeFillColor([0.0135,0.0177,0.0225,0.0291,0.0409,0.0716], 'PerFuPEV', pevColors)
   },
   'PA-FC-KD-SM': {
-    id: 'PA-FC-KD-SM',
     sourceLayer: 'pa_pev_bg',
-    fillColor: wpStyle([], 'FC_KD_SM')
+    fillColor: makeFillColor([], 'FC_KD_SM', wpColors)
   },
   'PA-FC-KD-JB': {
-    id: 'PA-FC-KD-JB',
     sourceLayer: 'pa_pev_bg',
-    fillColor: wpjobStyle([], 'FC_KD_JB')
+    fillColor: makeFillColor([], 'FC_KD_JB', wpjobColors)
   },
   'PA-FC-CE-SM': {
-    id: 'PA-FC-CE-SM',
     sourceLayer: 'pa_pev_bg',
-    fillColor: wpStyle([], 'FC_CE_SM')
+    fillColor: makeFillColor([], 'FC_CE_SM', wpColors)
   },
   'PA-FC-CE-JB': {
-    id: 'PA-FC-CE-JB',
     sourceLayer: 'pa_pev_bg',
-    fillColor: wpjobStyle([], 'FC_CE_JB')
+    fillColor: makeFillColor([], 'FC_CE_JB', wpjobColors)
   },
   'PA-PC-KD-SM': {
-    id: 'PA-PC-KD-SM',
     sourceLayer: 'pa_pev_bg',
-    fillColor: wpStyle([], 'PC_KD_SM')
+    fillColor: makeFillColor([], 'PC_KD_SM', wpColors)
   },
   'PA-PC-KD-JB': {
-    id: 'PA-PC-KD-JB',
     sourceLayer: 'pa_pev_bg',
-    fillColor: wpjobStyle([], 'PC_KD_JB')
+    fillColor: makeFillColor([], 'PC_KD_JB', wpjobColors)
   },
   'PA-PC-CE-SM': {
-    id: 'PA-PC-CE-SM',
     sourceLayer: 'pa_pev_bg',
-    fillColor: wpStyle([], 'PC_CE_SM')
+    fillColor: makeFillColor([], 'PC_CE_SM', wpColors)
   },
   'PA-PC-CE-JB': {
-    id: 'PA-PC-CE-JB',
     sourceLayer: 'pa_pev_bg',
-    fillColor: wpjobStyle([], 'PC_CE_JB')
+    fillColor: makeFillColor([], 'PC_CE_JB', wpjobColors)
   },
 
   // New Jersey
   'NJ-CurrentPEV-BG': {
-    id: 'NJ-CurrentPEV-BG',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'CurPEV')
+    fillColor: makeFillColor([], 'CurPEV', pevColors)
   },
   'NJ-CurrentPEV-Pop': {
-    id: 'NJ-CurrentPEV-Pop',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'CuPEVPop')
+    fillColor: makeFillColor([], 'CuPEVPop', pevColors)
   },
   'NJ-CurrentPEV-HH': {
-    id: 'NJ-CurrentPEV-HH',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'CuPEV_HU')
+    fillColor: makeFillColor([], 'CuPEV_HU', pevColors)
   },
   'NJ-CurrentPEV-SM': {
-    id: 'NJ-CurrentPEV-SM',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'CuPEV_SM')
+    fillColor: makeFillColor([], 'CuPEV_SM', pevColors)
   },
   'NJ-CurrentPEV-Veh': {
-    id: 'NJ-CurrentPEV-Veh',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'PerCuPEV')
+    fillColor: makeFillColor([], 'PerCuPEV', pevColors)
   },
   'NJ-FuturePEV-BG': {
-    id: 'NJ-FuturePEV-BG',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'FutPEV')
+    fillColor: makeFillColor([], 'FutPEV', pevColors)
   },
   'NJ-FuturePEV-Pop': {
-    id: 'NJ-FuturePEV-Pop',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'FuPEVPop')
+    fillColor: makeFillColor([], 'FuPEVPop', pevColors)
   },
   'NJ-FuturePEV-HH': {
-    id: 'NJ-FuturePEV-HH',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'FuPEV_HU')
+    fillColor: makeFillColor([], 'FuPEV_HU', pevColors)
   },
   'NJ-FuturePEV-SM': {
-    id: 'NJ-FuturePEV-SM',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'FUPEV_SM')
+    fillColor: makeFillColor([], 'FUPEV_SM', pevColors)
   },
   'NJ-FuturePEV-Veh': {
-    id: 'NJ-FuturePEV-Veh',
     sourceLayer: 'nj_pev_bg',
-    fillColor: pevStyle([], 'PerFuPEV')
+    fillColor: makeFillColor([], 'PerFuPEV', pevColors)
   },
   'NJ-FC-KD-SM': {
-    id: 'NJ-FC-KD-SM',
     sourceLayer: 'nj_pev_bg',
-    fillColor: wpStyle([12.29,28.98,56.37,100.15,164.75,321.26], 'FC_KD_SM')
+    fillColor: makeFillColor([12.29,28.98,56.37,100.15,164.75,321.26], 'FC_KD_SM', wpColors)
   },
   'NJ-FC-KD-JB': {
-    id: 'NJ-FC-KD-JB',
     sourceLayer: 'nj_pev_bg',
-    fillColor: wpjobStyle([0.0546,0.074,0.0898,0.1046,0.1228,0.1517], 'FC_KD_JB')
+    fillColor: makeFillColor([0.0546,0.074,0.0898,0.1046,0.1228,0.1517], 'FC_KD_JB', wpjobColors)
   },
   'NJ-FC-CE-SM': {
-    id: 'NJ-FC-CE-SM',
     sourceLayer: 'nj_pev_bg',
-    fillColor: wpStyle([1.802,4.2266,8.1766,14.1452,23.9576,44.3198], 'FC_CE_SM')
+    fillColor: makeFillColor([1.802,4.2266,8.1766,14.1452,23.9576,44.3198], 'FC_CE_SM', wpColors)
   },
   'NJ-FC-CE-JB': {
-    id: 'NJ-FC-CE-JB',
     sourceLayer: 'nj_pev_bg',
-    fillColor: wpjobStyle([0.0083,0.0108,0.0128,0.0145,0.0169,0.0209], 'FC_CE_JB')
+    fillColor: makeFillColor([0.0083,0.0108,0.0128,0.0145,0.0169,0.0209], 'FC_CE_JB', wpjobColors)
   },
   'NJ-PC-KD-SM': {
-    id: 'NJ-PC-KD-SM',
     sourceLayer: 'nj_pev_bg',
-    fillColor: wpStyle([8.52,20.03,39.14,69.46,114.29,222.67], 'PC_KD_SM')
+    fillColor: makeFillColor([8.52,20.03,39.14,69.46,114.29,222.67], 'PC_KD_SM', wpColors)
   },
   'NJ-PC-KD-JB': {
-    id: 'NJ-PC-KD-JB',
     sourceLayer: 'nj_pev_bg',
-    fillColor: wpjobStyle([0.0378,0.0512,0.0623,0.0724,0.0853,0.1052], 'PC_KD_JB')
+    fillColor: makeFillColor([0.0378,0.0512,0.0623,0.0724,0.0853,0.1052], 'PC_KD_JB', wpjobColors)
   },
   'NJ-PC-CE-SM': {
-    id: 'NJ-PC-CE-SM',
     sourceLayer: 'nj_pev_bg',
-    fillColor: wpStyle([1.2,2.83,5.48,9.5,15.96,29.68], 'PC_CE_SM')
+    fillColor: makeFillColor([1.2,2.83,5.48,9.5,15.96,29.68], 'PC_CE_SM', wpColors)
   },
   'NJ-PC-CE-JB': {
-    id: 'NJ-PC-CE-JB',
     sourceLayer: 'nj_pev_bg',
-    fillColor: wpjobStyle([0.0056,0.0072,0.0085,0.0097,0.0113,0.014], 'PC_CE_JB')
+    fillColor: makeFillColor([0.0056,0.0072,0.0085,0.0097,0.0113,0.014], 'PC_CE_JB', wpjobColors)
   }
 }
 
@@ -385,7 +283,7 @@ const makeSecondaryMapLayer = name => {
   // }
 
   return {
-      'id': layerInfo.id,
+      'id': name,
       'type': 'fill',
       'source': 'pev',
       'source-layer': layerInfo.sourceLayer,
@@ -410,7 +308,6 @@ const makeSecondaryMapLayer = name => {
 // for this project, overlay layers are secondaryMapLayers
 const secondaryMapLayers = {
   'charging': {
-    id: 'charging',
     type: 'circle',
     source: 'charging',
     paint: {
