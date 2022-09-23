0a941d9496e58da306366b417aa625503c21febe
mmolta <mcmolta10@gmail.com>
2022-09-22 20:49:08 -0400

    draw mcd and county boundaries on top

--- js/map/mapLayers.js
+++ js/map/mapLayers.js
@@ -7,35 +7,6 @@ let pev6 = '#2b8cbe';
 let pev7 = '#08589e';
 
 const layers = {
-  countyOutline: {
-    id: "county-outline",
-    type: "line",
-    source: "census",
-    "source-layer": "county",
-    paint: {
-      "line-width": 2.5,
-      "line-color": "#505a5e",
-    },
-    filter: ["==", "DVRPC", "Yes"],
-  },
-  muniOutline: {
-    id: "municipality-outline",
-    type: "line",
-    source: "census",
-    "source-layer": "municipalities",
-    paint: {
-      "line-width":{
-        base: 9,
-        stops: [
-          [8, 1],
-          [12, 3],
-          [13, 5],
-        ],
-      },
-      "line-color": "#4a5c64",
-    },
-    filter: ["==", "DVRPC", "Yes"],
-  },
   'DVRPC-CurrentPEV-Pop': {
     'id': 'DVRPC-CurrentPEV-Pop',
     'type': 'fill',
@@ -183,6 +154,35 @@ const layers = {
     },
     "filter": ["==", "geoid10", ''],
   },
+  countyOutline: {
+    id: "county-outline",
+    type: "line",
+    source: "census",
+    "source-layer": "county",
+    paint: {
+      "line-width": 2.5,
+      "line-color": "#505a5e",
+    },
+    filter: ["==", "DVRPC", "Yes"],
+  },
+  muniOutline: {
+    id: "municipality-outline",
+    type: "line",
+    source: "census",
+    "source-layer": "municipalities",
+    paint: {
+      "line-width":{
+        base: 9,
+        stops: [
+          [8, 1],
+          [12, 3],
+          [13, 5],
+        ],
+      },
+      "line-color": "#4a5c64",
+    },
+    filter: ["==", "DVRPC", "Yes"],
+  }
 };
 
 export default layers;
