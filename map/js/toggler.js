f6b3e732b8d8640e87579e68820c0135fe4201e6
ckirby98 <colin@kirby.net>
2024-08-26 14:35:18 -0400

    removed pev future/current dropdown, updated text to remove info about future projections

--- map/js/toggler.js
+++ map/js/toggler.js
@@ -1,28 +1,32 @@
 const togglerPEV = (pevToggle, wpToggle) => {
-    $('.distribution').show();
-    $('.pev').show();
-    $('.workplace').hide();
-    $('.charge').hide();
+  $(".distribution").show();
+  $(".pev").show();
+  $("#type_select").hide();
+  $(".workplace").hide();
+  $(".charge").hide();
 
-    document.getElementById('type_select').selectedIndex=0;
-    document.getElementById('layout_select').selectedIndex = document.getElementById('pevTop').index;
+  document.getElementById("type_select").selectedIndex = 0;
+  document.getElementById("layout_select").selectedIndex =
+    document.getElementById("pevTop").index;
 
-    wpToggle.classList.remove('toggle-btn-active');
-    pevToggle.classList.add('toggle-btn-active');
+  wpToggle.classList.remove("toggle-btn-active");
+  pevToggle.classList.add("toggle-btn-active");
 };
 
-// WorkPlace 
+// WorkPlace
 const togglerWP = (pevToggle, wpToggle) => {
-    $('.workplace').show();
-    $('.charge').show();
-    $('.distribution').hide();
-    $('.pev').hide();
+  $(".workplace").show();
+  $(".charge").show();
+  $("#type_select").show();
+  $(".distribution").hide();
+  $(".pev").hide();
 
-    document.getElementById('type_select').selectedIndex=2;
-    document.getElementById('layout_select').selectedIndex = document.getElementById('chargeTop').index;
+  document.getElementById("type_select").selectedIndex = 2;
+  document.getElementById("layout_select").selectedIndex =
+    document.getElementById("chargeTop").index;
 
-    pevToggle.classList.remove('toggle-btn-active');
-    wpToggle.classList.add('toggle-btn-active');
+  pevToggle.classList.remove("toggle-btn-active");
+  wpToggle.classList.add("toggle-btn-active");
 };
 
 export { togglerPEV, togglerWP };
